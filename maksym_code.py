2332a71e7d22b369422795c7f4717e50593c1374
maxpasichnyk <maksym.pasichnyk.25@gmail.com>
2026-07-22 14:45:07 +0300

    add a function to create signals

--- maksym_code.py
+++ maksym_code.py
@@ -1,11 +1,3 @@
-import yfinance as yf
-import pandas as pd
-data = yf.download(
-    "AAPL",
-    start="2020-01-01",
-    end="2026-07-01"
-)
-df=pd.DataFrame(data)
 def clean_data(df):
     df=df.dropna()
     df = df.drop_duplicates()
@@ -14,3 +6,19 @@ def clean_data(df):
 def calculate_moving_average(df):
     df["Short_MA"]=df["Close"].rolling(window=20).mean()
     df["Long_MA"]=df["Close"].rolling(window=50).mean()
+    df.dropna()
+    return df
+def signals(df):
+    df["Signal"] = "Hold"
+    for i in range(1, len(df)):
+        today_short=df.iloc[i]["Short_MA"]
+        today_long=df.iloc[i]["Long_MA"]
+        yesterday_short=df.iloc[i-1]["Short_MA"]
+        yesterday_long=df.iloc[i-1]["Long_MA"]
+        # for signal "BUY"
+        if today_short>today_long and yesterday_short<=yesterday_long:
+            df.loc[df.index[i], "Signal"] = "BUY"
+        elif today_short<today_long and yesterday_short>=yesterday_long:
+            df.loc[df.index[i], "Signal"] = "SELL"
+    return df
+
