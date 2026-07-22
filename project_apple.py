aa0aa42f2bfddcbb121f4bb537e59dfac6624e7c
syurkiv <sofiia.yurkiv.25@kse.org.ua>
2026-07-22 16:31:20 +0300

    added functions for our strategy calculations and p&l calculations

--- project_apple.py
+++ project_apple.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import pandas as pd
 import maksym_code
+import sonya_code
+import csv
 data = yf.download(
     "AAPL",
     start="2020-01-01",
@@ -13,3 +15,10 @@ df=maksym_code.calculate_moving_average(df)
 df=maksym_code.signals(df)
 with open("extra files to project/applebuysell.csv", "w") as file:
     df.to_csv(file)
+# частина Соні
+df, profits = sonya_code.calculate_pnl(df)
+print(profits)
+strategy = sonya_code.strategy(profits)
+print(strategy)
+
+
